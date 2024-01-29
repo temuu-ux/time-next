e1383eee2f12909d95eae13ed52cbf1598dbce77
Temuulen Batbileg <23LP8360@G304-CC0074.local>
2024-01-29 19:10:01 +0800

    hi

--- src/pages/stop.js
+++ src/pages/stop.js
@@ -1,32 +1,55 @@
-import { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-function stop() {
+const Stopwatch = () => {
+  // state to store time
   const [time, setTime] = useState(0);
 
-  setInterval(() => setTime(time+1), 1000);
+  // state to check stopwatch running or not
+  const [isRunning, setIsRunning] = useState(false);
 
+  useEffect(() => {
+    let timer;
+    if (isRunning) {
+      // setting time from 0 to 1 every 10 milisecond using javascript setInterval method
+      timer = setInterval(() => setTime(time + 1), 10);
+      console.log(timer, "his");
+    }
+    return () => clearInterval(timer);
+  }, [isRunning, time]);
 
-//   Math
+  const hours = Math.floor(time / 360000);
 
+  const minutes = Math.floor((time % 360000) / 6000);
+
+  const seconds = Math.floor((time % 6000) / 100);
+
+  const milliseconds = time % 100;
+
+  const startAndStop = () => {
+    setIsRunning(!isRunning);
+  };
+
+  const reset = () => {
+    setTime(0);
+    setIsRunning(0);
+  };
   return (
-    <div className="flex flex-col gap-8 text-center justify-center">
-      <div className="flex justify-center">
-        <p>00:</p>
-        <p>00:</p>
-        <p>{time}</p>
-      </div>
-      <div className="flex justify-center gap-3 ">
-        <button className="border-solid border-2 p-2">
-          <p>start</p>
+    <div className="flex flex-col justify-center items-center">
+      <p className="">
+        {hours}:{minutes.toString().padStart(2, "0")}:
+        {seconds.toString().padStart(2, "0")}:
+        {milliseconds.toString().padStart(2, "0")}
+      </p>
+      <div className="flex gap-3 ">
+        <button className="stopwatch-button border-2" onClick={startAndStop}>
+          {isRunning ? "Stop" : "Start"}
         </button>
-        <button className="border-solid border-2 p-2">
-          <p>reset</p>
-        </button>
-        <button className="border-solid border-2 p-2">
-          <p>stop</p>
+        <button className="stopwatch-button border-2" onClick={reset}>
+          Reset
         </button>
       </div>
     </div>
   );
-}
-export default stop;
+};
+
+export default Stopwatch;
